8dad1c4d124b90ffd1626e7e8b64f339c938d09a
Ian Nater <ian.nater@gmail.com>
2020-10-30 17:03:55 -0500

    have the cards populating from the db on the members page for whoever is logged in. I tried to get handlebars working but couldnt. Leaving the files here for now incase someone wants to look

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
+//Requiring handle bars
+const hbs = require("express-handlebars");
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
+// Adding Handlebars as the engine
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
